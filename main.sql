06a58665d8dd3fd7fee52d0e72733533fce4b7bf
glenlouis-lgtm <glenlouis@gmail.com>
2026-06-21 19:49:21 +0100

    Add files via upload

---
+++ main.sql
@@ -0,0 +1,6 @@
+INSERT INTO Employees (EmployeeID, EmployeeName, Department, Salary)
+VALUES
+(101, 'John', 'IT', 55000),
+(102, 'Mary', 'HR', 45000),
+(103, 'David', 'IT', 60000),
+(104, 'Sarah', 'Finance', 50000);
